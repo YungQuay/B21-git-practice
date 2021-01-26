cf66919095e0cbf10f797245811e9ad09faa6a23
Ruslan Petrov <richhomiequan94@mail.ru>
2021-01-26 15:16:19 -0500

    movie1 branch updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World, B21");
         System.out.println("b21 future SDET learning GIT ");
     }
 }
