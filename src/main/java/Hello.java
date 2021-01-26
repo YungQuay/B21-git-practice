0932088f207d913aeb0c208f9a0f80217bf5019c
Ruslan Petrov <richhomiequan94@mail.ru>
2021-01-26 15:25:02 -0500

    movie1 branch final work is done, ready to merge

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, B22");
         System.out.println("b21 future SDET learning GIT ");
-        //watching more movie
+        System.out.println("Ready go back to living room");
     }
 }
